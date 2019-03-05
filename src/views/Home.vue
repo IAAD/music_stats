94571a8af651671689b768d2ec9bf70e45b583d5
ovuoba sampson <ovuobasampson@gmail.com>
2019-03-04 20:18:24 -0800

    trying to bypass cors

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -22,11 +22,26 @@ export default {
       tracks: []
     };
   },
-  tracks() {
-    axios
-      .get()
-      .then()
-      .catch();
+  mounted() {
+    const session_url = 'https://accounts.spotify.com/api/token';
+    const username = '88ab0ee5549d4d5282676a11604f2545';
+    const password = 'c96c13c6d3104b6299a4a7765d76f642';
+    const basicAuth = 'Basic ' + btoa(username + ':' + password);
+    const requestBody ={
+      grant_type: 'client_credentials'
+    }
+    const config = {
+      headers: {
+        'Content-Type':'application/x-www-form-urlencoded',
+        'Access-Control-Allow-Origin':'*',
+        'Authorization': basicAuth
+      }
+    }
+    axios.post(session_url, requestBody, config).then(function(response) {
+      console.log(response.data.access_token);
+    }).catch(function(err) {
+      console.log(err);
+    })
   }
 };
 </script>
