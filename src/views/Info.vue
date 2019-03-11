fd9e67253f30b444dd98b76fb648468cd899ff91
ovuoba sampson <ovuobasampson@gmail.com>
2019-03-11 16:01:10 -0700

    added graph input

--- src/views/Info.vue
+++ src/views/Info.vue
@@ -98,7 +98,7 @@ export default {
         },
         xaxis: {
           categories: [
-                  "svfs",   "svfs" ,  "svfs"  , "svfs",
+                  "svfs",   "svfs" ,  "sgdhe"  , "svfs",
             "svfs",   "svfs",   "svfs",   "svfs",   "svfs",   "tewutuwr"
           ]
         }
