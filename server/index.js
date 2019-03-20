3ac1c41c12324d3d61bc5328b695a75d36cb8188
ovuoba sampson <ovuobasampson@gmail.com>
2019-03-20 05:44:56 -0700

    prepped the server for production added spa handlers

--- server/index.js
+++ server/index.js
@@ -1,28 +1,23 @@
-
-const express = require('express');
-const cors = require('cors');
-const api = require('./router/api');
+const express = require("express");
+const cors = require("cors");
+const api = require("./router/api");
 
 const app = express();
 
 app.use(cors());
-app.use('/api',api)
+app.use("/api", api);
 
-// //Handle Production
-// if (process.env.NODE_ENV === 'production'){
-//   //Static folder
-//   app.use(express.static(__dirname + '/public/'));
-//
-//   //Handle SPA route
-//   app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'))
-// }
+//Handle Production
+if (process.env.NODE_ENV === "production") {
+  //Static folder
+  app.use(express.static(__dirname + "/public/"));
+
+  //Handle SPA route
+  app.get(/.*/, (req, res) => res.sendFile(__dirname + "/public/index.html"));
+}
 
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
   console.log(`Server running at Port ${port}`);
 });
-
-
-
-
